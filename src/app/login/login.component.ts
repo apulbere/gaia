d7dbec1b8a3bfb90acc0e2e01a399ff606db9365
Adrian Pulbere <apulbere@yahoo.com>
2017-10-21 00:58:02 +0300

    update style and readme

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,8 +4,8 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { AlertService, AuthenticationService } from '../_services/index';
 
 @Component({
-    moduleId: module.id,
-    templateUrl: 'login.component.html'
+    templateUrl: 'login.component.html',
+    styleUrls: ['./login.component.css']
 })
 
 export class LoginComponent implements OnInit {
@@ -20,10 +20,7 @@ export class LoginComponent implements OnInit {
         private alertService: AlertService) { }
 
     ngOnInit() {
-        // reset login status
         this.authenticationService.logout();
-
-        // get return url from route parameters or default to '/'
         this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
     }
 
